12f00e5f8f9cd88182cfed18563230caf3cce03c
Hong Yifan <hyfing@gmail.com>
2024-06-17 20:51:59 +0800

    Add vterm support.
    Install emacs-vterm package by apt.

---
+++ lisp/init-vterm.el
@@ -0,0 +1,11 @@
+;;; init-elpa.el --- Settings and helpers for package.el -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; (add-hook 'term-setup-hook 'vterm-initialize)
+;; (setq terminal-frame-alist '((:terminal . vterm-mode)))
+;; (setq terminal-emulator "xterm-256color")
+;; (setq exec-path (cons "/usr/lib" exec-path))
+(require 'vterm)
+
+(provide 'init-vterm)
