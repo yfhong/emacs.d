70f07b4f196ef62b3818f49f6704266d9b800acc
Hong Yifan <hyfing@gmail.com>
2026-02-05 19:30:59 +0800

    Configure default and Chinese fonts
    Set default font to a generic serif family and Chinese characters to "宋体"
    with a consistent size of 14pt. This change respects user preference
    for serif fonts and ensures unified font sizing across character sets.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,6 +3,16 @@
 (when is-windows
   (require 'init-mswin))
 
+;; Set default font and size (English part unchanged, size set to 14pt)
+(set-face-attribute 'default nil :family "Serif" :height 140) ; Set default font to 14pt, preferring serif
+
+;; Set Chinese font to Song Ti and ensure its size is also 14pt
+(set-fontset-font "fontset-default" '((cn-gb-2312 . "Song Ti")
+                                      (cn-gb-extended . "Song Ti")
+                                      (kana . "Song Ti"))
+                  nil 'append)
+(set-fontset-font "fontset-default" 'unicode "Song Ti") ; Ensure unicode characters use Song Ti where applicable
+
 (condition-case nil
     (progn
       (require 'init-vterm)
